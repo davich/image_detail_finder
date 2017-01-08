4a4bff5152c5ddc2ccfbb61845d5a8bb474ac757
David Carlin <david@projectproject.com.au>
2017-01-09 10:26:40 +1100

    Refactor move finding details coordinate into method

--- detail.rb
+++ detail.rb
@@ -3,19 +3,26 @@ def calculate_square_size(filename)
 end
 
 def rectangle_coords(point_coords, square_size)
-  "%d,%d %d,%d" % [point_coords[0]-square_size/2, point_coords[1]-square_size/2, point_coords[0]+square_size/2, point_coords[1]+square_size/2]
+  half_square = square_size/2
+  coord_array = [
+    point_coords[0] - half_square,
+    point_coords[1] - half_square,
+    point_coords[0] + half_square,
+    point_coords[1] + half_square
+  ]
+  "%d,%d %d,%d" % coord_array
 end
 
-filename = ARGV[0]
-`convert #{filename} -edge 1 #{filename}.tmp1`
-`convert #{filename}.tmp1 -statistic Mean 5x5 #{filename}.tmp2`
-result = `identify -define identify:locate=maximum -define identify:limit=2 #{filename}.tmp2`
-coords = result[/(\d+,\d+)\n/, 1].split(',').map(&:to_i)
+def detailed_pixel_coords(filename)
+  `convert #{filename} -edge 1 #{filename}.tmp1`
+  `convert #{filename}.tmp1 -statistic Mean 5x5 #{filename}.tmp2`
+  result = `identify -define identify:locate=maximum -define identify:limit=2 #{filename}.tmp2`
+  `rm #{filename}.tmp*`
+  result[/(\d+,\d+)\n/, 1].split(',').map(&:to_i)
+end
+
+coords = detailed_pixel_coords(ARGV[0])
 
 square_size = calculate_square_size(filename)
 
 `convert #{filename} -stroke red -strokewidth 1 -fill none -draw "rectangle #{rectangle_coords(coords, square_size)} " out-#{filename}`
-
-
-`rm #{filename}.tmp*`
-# `open out-#{filename}`
