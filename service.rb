7b90903477f2380dbcd32f63f2109c549b3b7c3c
David Carlin <davich@gmail.com>
2017-01-24 07:30:09 +1100

    cleaned up files, combined commands

--- service.rb
+++ service.rb
@@ -10,20 +10,22 @@ class Service
 
   def run
     t = Thread.new do
-      `curl -o input.png #{small_url}`
-      idf = ImageDetailFinder.new('input.png')
+      `curl -o #{input_filename} #{small_url}`
+      idf = ImageDetailFinder.new(input_filename)
       crop_coords = idf.rectangle_coords_for_crop
+      `rm #{input_filename}`
+      crop_coords
     end
 
-    `curl -o original.png "#{Imagehaus.image_url(@image_id)}"`
-    dims = image_dimensions('original.png')
+    `curl -o #{original_filename} "#{Imagehaus.image_url(@image_id)}"`
+    dims = image_dimensions(original_filename)
 
     t.join
     crop_coords = t.value.map {|i| i*dims.max/550 }
 
     rect = "%dx%d+%d+%d" % crop_coords
-    `convert -crop #{rect} original.png cropped.png`
-    `convert cropped.png -resize 800x800\\> cropped_resized.png`
+    `convert #{original_filename} -crop #{rect} -resize 800x800\\> result.png`
+    `rm #{original_filename}`
     puts "done"
   end
 
@@ -34,7 +36,16 @@ class Service
   def small_url
     "http://ih1.redbubble-staging.net/image.#{@image_id}.#{@work_id}/flat,550x550,075,f.png"
   end
+
+  def input_filename
+    "#{@image_id}-#{@work_id}-input.#{@format}"
+  end
+
+  def original_filename
+    "#{@image_id}-#{@work_id}-original.#{@format}"
+  end
 end
 
-Service.new(7188853, '0690').run
+Service.new(7198014, 1623).run
+# Service.new(7188853, '0690').run
 # Service.new(7414867, 2906).run
